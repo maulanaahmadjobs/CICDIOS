56999d7963eecb427212e203ff8db51836af810f
Maulana West <maulanaahmadjobs@gmail.com>
2023-10-09 11:21:54 +0700

    Initial Commit

---
+++ RetirementCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RetirementCalculator
+//
+//  Created by west on 09/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
